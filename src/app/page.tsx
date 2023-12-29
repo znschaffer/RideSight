37c3430e46617b7251ba80e154751479c0223f38
Zane Schaffer <znschaffer@gmail.com>
2023-12-28 22:34:39 -0800

    update styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 "use client";
 import Map from "react-map-gl/maplibre";
 import MapContainer from "@/components/MapContainer";
-import AuthFlow from "@/components/AuthFlow"
+import AuthFlow from "@/components/AuthFlow";
 
 export default function Home() {
   return (
-    <div style={{ height: "100dvh", display: "flex" }}>
-      <nav>
-        <h1>RideSight</h1>
-      </nav>
+    <div className="h-full w-full grid grid-cols-12 grid-rows-2">
+      <div className="col-span-5 w-full h-full m-auto p-10 flex flex-col justify-center align-center outline-black  ">
+        <h1 className=" lg:text-8xl md:text-6xl sm:text-2xl text-center">
+          RideSight
+        </h1>
+        <p className="text-2xl text-gray-600 text-center pt-10">
+          It's Time To See What You've Done
+        </p>
+      </div>
       <AuthFlow />
+      <div className="w-full h-full col-span-5 flex flex-col justify-center align-center outline-black ">
+        <p className="text-center">Placeholder</p>
+      </div>
+      <div className="w-full h-full col-span-7 flex flex-col justify-center align-center outline-black ">
+        <p className="text-center">Placeholder</p>
+      </div>
     </div>
   );
 }
--- src/components/AuthFlow.tsx
+++ src/components/AuthFlow.tsx
@@ -1,11 +1,35 @@
-
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
 import { env } from "process";
 
 export default function AuthFlow() {
-  const CALLBACK_URL = "https://ridesight.znschaffer.com"
-  return (<>
-
-    <h1>Sign In With Strava</h1>
-    <a href={`http://www.strava.com/oauth/authorize?client_id=118783&response_type=code&redirect_uri=${CALLBACK_URL}/map&approval_prompt=force&scope=activity:read_all`}>Log In</a>
-  </>)
+  const CALLBACK_URL = "https://ridesight.znschaffer.com";
+  return (
+    <div className="flex w-full h-full flex-col p-10  outline-black col-span-7 justify-center items-center align-center">
+      <Card className="w-fit m-auto">
+        <CardHeader>
+          <CardTitle>Sign In With Strava</CardTitle>
+          <CardDescription>Import Your Ride Data</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div>
+            <Button asChild className="m-auto">
+              <Link
+                href={`http://www.strava.com/oauth/authorize?client_id=118783&response_type=code&redirect_uri=${CALLBACK_URL}/map&approval_prompt=force&scope=activity:read_all`}
+              >
+                Log In
+              </Link>
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
